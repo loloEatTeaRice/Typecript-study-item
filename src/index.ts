ac6e962db6fa5968f9270820c7d20191abd516a7
lolo <18902276190@163.com>
2023-04-17 11:20:07 +0800

    比较完整的ts打包配置

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,7 @@ function sum(a: number, b:number): number {
     return a + b;
 }
 
+const a = 1;
+
 console.log(sum(123, 456));
+console.log('哈哈哈');
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
 const path = require("path");
-// const HtmlWebpackPlugin = require("html-webpack-plugin");
-// const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
+    mode: 'development',
+
     // optimization: {
     //     minimize: false // 关闭代码压缩，可选
     // },
@@ -11,21 +13,26 @@ module.exports = {
 
     // devtool: "inline-source-map",
 
-    // devServer: {
-    //     contentBase: './dist'
-    // },
+    devServer: {
+        // static: './dist'
+        open: {
+            app: {
+                name: 'chrome'
+            }
+        }
+    },
 
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "bundle.js",
-        // environment: {
-        //     arrowFunction: false // 关闭webpack的箭头函数，可选
-        // }
+        environment: {
+            arrowFunction: false // 关闭webpack的箭头函数，可选
+        }
     },
 
-    // resolve: {
-    //     extensions: [".ts", ".js"]
-    // },
+    resolve: {
+        extensions: [".ts", ".js"]
+    },
 
     // webpack打包时要使用的模块
     module: {
@@ -33,17 +40,37 @@ module.exports = {
         rules: [{
             // test 指定规则生效的文件
             test: /\.ts$/,
-            use: {
-                loader: "ts-loader"
+            use: [{
+                loader: "babel-loader",
+                options: {
+                    // 设置预定义的环境
+                    presets: [
+                        [
+                            "@babel/preset-env",
+                            {
+                                "targets": {
+                                    "chrome": "58",
+                                    "ie": "11"
+                                },
+                                "corejs": "3",
+                                "useBuiltIns": "usage"
+                            }
+                        ]
+                    ]
+                }
             },
+            {
+                loader: "ts-loader",
+            }
+            ],
             exclude: /node_modules/
         }]
     },
 
-    // plugins: [
-    //     new CleanWebpackPlugin(),
-    //     new HtmlWebpackPlugin({
-    //         title: 'TS测试'
-    //     }),
-    // ]
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'TS测试'
+        }),
+    ]
 }
